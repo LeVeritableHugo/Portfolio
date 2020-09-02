e7807f02e9b78a51ce10c89a126a98499cf776e9
LeVeritableHugo <thehugo56@gmail.com>
2020-09-03 00:30:36 +0200

    Portfolio v0.2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let collapsed = true;
 let iconID = 0;
 
+
 const bouton = document.querySelector(".expand");
 let menuIcon = document.querySelector('.menuIcon');
 //Menu déroulant
@@ -8,23 +9,38 @@ function expand()
 {
     document.querySelector('.wrapper-sidebar').classList.toggle("collapsed")
     document.querySelector('.wrapper-menu').classList.toggle("hidden");
+    document.querySelector('.contactIcon').classList.toggle("hidden")
+
     if(!collapsed)
     {
         // bouton.textContent = ">";
-        iconID = 1;
-        menuIcon.src = 'img/Icone-' + iconID + '.png';
+        menuIcon.src = 'img/Icones/Icone-1.png';
         collapsed = true;
 
     }
     else
     {
         // bouton.textContent = "X";
-        iconID = 0;
-        menuIcon.src = 'img/Icone-' + iconID + '.png';
+        menuIcon.src = 'img/Icones/Icone-0.png';
         collapsed = false;
-
-
     }
 }
+
+let posImage = 0;
+let walkAnim = document.querySelector(".walkingAnim");
+
+function walking() 
+{
+  walkAnim.style.paddingLeft= (posImage) + "px";
+  posImage += 5;
+  if(posImage >= 2000)
+  {
+      posImage = 0;
+  }
+
+}
+
+document.addEventListener('scroll', walking)
+
 
 bouton.addEventListener('click', expand);
