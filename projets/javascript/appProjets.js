d4cfb71ef9ee5c00e02adb9dfb2300e668dc5d8d
LeVeritableHugo <thehugo56@gmail.com>
2020-09-11 23:59:35 +0200

    Portfolio v0.66

--- projets/javascript/appProjets.js
+++ projets/javascript/appProjets.js
@@ -1,11 +1,12 @@
+
+
 // Images de preview projet 
 let imagePreviewDice = document.querySelector('.dicegameAccess');
 let imageGitDice = document.querySelector('.dicegameAccess2');
 let imagePreviewBlackjack = document.querySelector('.blackjackAccess');
 let imageGitBlackjack = document.querySelector('.blackjackAccess2');
-
-
-
+let imagePreviewPf = document.querySelector('.portfolioAccess');
+let imageGitPf = document.querySelector('.portfolioAccess2');
 
 function voirGithub(elem)
 {
@@ -18,7 +19,6 @@ function voirGithub(elem)
     })
 }
 
-
 // Anim jeu de dés
 
     imagePreviewDice.addEventListener('mouseenter', function(){
@@ -29,7 +29,47 @@ function voirGithub(elem)
         imagePreviewDice.src = 'dicegame/previewImages/animationblur.png';
     })
 
-voirGithub(imageGitDice);
+    voirGithub(imageGitDice);
+    
+    // Anim Fond
+    window.addEventListener('scroll', () =>{
+ 
+        // Dés 1 à 5
+        if(window.scrollY < 300)
+        {
+            document.getElementById("dicegame").style.backgroundImage = "url('dicegame/previewImages/animBG1.png')";
+        }
+
+        if(window.scrollY > 300)
+        {
+            document.getElementById("dicegame").style.backgroundImage = "url('dicegame/previewImages/animBG2.png')";
+        }
+
+        if(window.scrollY > 500)
+        {
+            document.getElementById("dicegame").style.backgroundImage = "url('dicegame/previewImages/animBG3.png')";
+        }
+
+        if(window.scrollY > 700)
+        {
+            document.getElementById("dicegame").style.backgroundImage = "url('dicegame/previewImages/animBG4.png')";
+        }
+        if(window.scrollY > 900)
+        {
+            document.getElementById("dicegame").style.backgroundImage = "url('dicegame/previewImages/animBG5.png')";
+        }
+
+        // Titre Porfolio
+        if(window.scrollY <= 2172)
+        {
+            document.querySelector('.pfTitre').style.color = "#171717";
+        }
+        else
+        {
+            document.querySelector('.pfTitre').style.color = "#d7d1dd";
+
+        }        
+    })
 
 // Anim Blackjack
 
@@ -42,4 +82,16 @@ voirGithub(imageGitDice);
     })
 
     voirGithub(imageGitBlackjack);
+
+// Anim Porfolio
+
+imagePreviewPf.addEventListener('mouseenter', function(){
+    imagePreviewPf.src = 'portfolio/previewImages/preview2.png';
+})
+
+imagePreviewPf.addEventListener('mouseleave', function(){
+    imagePreviewPf.src = 'portfolio/previewImages/preview1.png';
+})
+
+voirGithub(imageGitPf);
 
